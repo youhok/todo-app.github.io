36b3984f318de30f7a4f1aa7d3ae90647eed53c8
Youhok Heng <youhokheng@Youhoks-MacBook-Pro.local>
2023-05-04 13:42:03 +0700

    update project

--- project.js
+++ project.js
@@ -1,42 +1,57 @@
 
-let button = document.getElementById("addtodo");
+//arr for labellist 
+let labellist = [];
 
+// statuslist for checkbox
+let statuslist = [];
 
-let show = document.getElementById("input");
-
-let answer = document.getElementById("answer");
-
-const parent = document.getElementById("answer");
-
-button.addEventListener("click" , function(){
-    var paragraph = document.createElement('p')
-    paragraph.innerText = input.value;
-    answer.appendChild(paragraph);
-
-
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.name = "myCheckbox";
-    checkbox.value = "myCheckboxValue";
-    parent.appendChild(checkbox);
-
-    if( checkbox === true){
-        return check ;
-    }else if(checkbox === false){
-        return "don't have";
+// function chick
+function addTodo() {
+    // get id from documentt = todo-label
+    let inputodolabel = document.getElementById("todo-label").value;
+    // show in ul
+    let ulTodo = document.getElementById("ul-todo");
+    let ulCompleted = document.getElementById("ul-completed");
+    if (labellist.length == 0 || labellist.length > 0 && !labellist.includes(inputodolabel)) {
+        //code for push somethingyou write in arr
+        labellist.push(inputodolabel);
+        //get the statuslist when you push in the arr to false
+        statuslist.push(false);
     }
-
-})
-
-
-
-let check = document.getElementById("check");
+    refreshList(ulTodo, ulCompleted);
+}
 
 
+function refreshList(ulTodo, ulCompleted) {
+    ulTodo.innerHTML = '';
+    ulCompleted.innerHTML = '';
+    console.log(statuslist);
+    //loop for check arr
+    for (let i = 0; i < labellist.length; i++) {
+        //give create li in arr
+        var listitem = document.createElement("li");
+        var checkbox = document.createElement('input');
 
-// if (c)
-// check.addEventListener("chang", function(){
+        checkbox.type = "checkbox";
+        checkbox.id = i;
+        checkbox.name = "checkbox";
+        checkbox.value = statuslist[i];
+        checkbox.onclick = function (res) {
+            let id = res.target.id;
+            statuslist[id] = !statuslist[id];
+            refreshList(ulTodo, ulCompleted);
+        };
+        // make a text content 
+        listitem.textContent = labellist[i];
 
-// })
-
+        if (statuslist[i] == false) {
+            //appendchild to show li in ul
+            ulTodo.appendChild(listitem);
+        } else {
+            ulCompleted.appendChild(listitem);
+            checkbox.checked = true;
+        }
+        // ul.appendChild(checkbox);
+        listitem.appendChild(checkbox);
+    }
+}
